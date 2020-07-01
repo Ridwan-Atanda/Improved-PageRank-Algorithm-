bc14662f4e8a0ca615c8ef3294cdcfd7bd3add34
ATANDA RIDWAN <atandaridwan@ATANDAs-MacBook-Pro.local>
2020-07-01 13:29:48 +0100

    added an update code

---
+++ src/main/java/com/data/mappers/LinkMapper.java
@@ -0,0 +1,84 @@
+package com.data.mappers;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.MapReduceBase;
+import org.apache.hadoop.mapred.Mapper;
+import org.apache.hadoop.mapred.OutputCollector;
+import org.apache.hadoop.mapred.Reporter;
+import org.bson.Document;
+
+import com.data.models.WebInfo;
+
+public class LinkMapper extends MapReduceBase implements Mapper<Object, Text, String, WebInfo> {
+
+	/**
+	 * Checks if the URL should be included in the link map
+	 * 
+	 * @param url The URL
+	 * @return The status
+	 */
+	private boolean isIncluded(String url) {
+
+		String file = url.substring(url.lastIndexOf("/") + 1);
+		String format = file.substring(file.lastIndexOf(".") + 1);
+		return (url.startsWith("http") || url.startsWith("https"))
+				&& (StringUtils.isBlank(format) || StringUtils.equalsAny("stm", "htm", "html", "shtml", format));
+	}
+
+	/**
+	 * Maps the links
+	 */
+	@Override
+	@SuppressWarnings("unchecked")
+	public void map(Object key, Text value, OutputCollector<String, WebInfo> output, Reporter reporter)
+			throws IOException {
+
+		try {
+			byte[] data = Files.readAllBytes(Paths.get(value.toString()));
+			String jsonContent = new String(data);
+			Document root = Document.parse(jsonContent);
+			Document envelope = (Document) root.get("Envelope");
+			if (envelope != null) {
+				Document headerMetaData = (Document) envelope.get("WARC-Header-Metadata");
+				if (headerMetaData != null) {
+					String sourceURL = headerMetaData.getString("WARC-Target-URI");
+					if (StringUtils.isNotBlank(sourceURL)) {
+						List<String> targetURLs = new ArrayList<>();
+						Document payloadMetaData = (Document) envelope.get("Payload-Metadata");
+						if (payloadMetaData != null) {
+							Document httpResponseMetadata = (Document) payloadMetaData.get("HTTP-Response-Metadata");
+							if (httpResponseMetadata != null) {
+								Document htmlMetaData = (Document) httpResponseMetadata.get("HTML-Metadata");
+								if (htmlMetaData != null) {
+									List<Document> links = (List<Document>) htmlMetaData.get("Links");
+									if (links != null && !links.isEmpty()) {
+										links.forEach(link -> {
+											String url = link.getString("url");
+											if (StringUtils.isNotBlank(url) && isIncluded(url)) {
+												targetURLs.add(url);
+											}
+										});
+									}
+								}
+							}
+						}
+
+						// Sending to reducer
+						if (!targetURLs.isEmpty()) {
+							output.collect("web-graph", new WebInfo(sourceURL, targetURLs));
+						}
+					}
+				}
+			}
+		} catch (IOException ioException) {
+			ioException.printStackTrace();
+		}
+	}
+}
---
+++ src/main/java/com/data/reducers/LinkReducer.java
@@ -0,0 +1,169 @@
+package com.data.reducers;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map.Entry;
+
+import org.apache.hadoop.mapred.MapReduceBase;
+import org.apache.hadoop.mapred.OutputCollector;
+import org.apache.hadoop.mapred.Reducer;
+import org.apache.hadoop.mapred.Reporter;
+
+import com.data.models.Connection;
+import com.data.models.WebInfo;
+import com.data.utils.LoopDetector;
+import com.data.utils.StaleLinkChecker;
+
+public class LinkReducer extends MapReduceBase implements Reducer<String, WebInfo, String, Double> {
+
+	/**
+	 * Gets the link index map
+	 * 
+	 * @param webInfoList The web info list
+	 * @return The link index map
+	 */
+	private HashMap<String, Integer> getLinkIndexMap(List<WebInfo> webInfoList) {
+
+		int index = 0;
+		HashMap<String, Integer> linkIndexMap = new HashMap<>();
+
+		for (WebInfo webInfo : webInfoList) {
+			String sourceURL = webInfo.getHadoopSourceURL();
+			List<String> targetURLs = webInfo.getHadoopTargetURLs();
+
+			if (!StaleLinkChecker.check(sourceURL)) {
+				linkIndexMap.put(sourceURL, index++);
+			}
+			for (String targetURL : targetURLs) {
+				if (!StaleLinkChecker.check(targetURL)) {
+					linkIndexMap.put(targetURL, index++);
+				}
+			}
+		}
+
+		return linkIndexMap;
+	}
+
+	/**
+	 * Prepares the adjacency matrix
+	 * 
+	 * @param webInfoList  The web info list
+	 * @param linkIndexMap The link index map
+	 * @return The adjacency matrix
+	 */
+	private int[][] prepareAdjacencyMatrix(List<WebInfo> webInfoList, HashMap<String, Integer> linkIndexMap) {
+
+		int totalLinks = linkIndexMap.size();
+		int[][] adjacency = new int[totalLinks][totalLinks];
+
+		for (WebInfo webInfo : webInfoList) {
+			String sourceURL = webInfo.getHadoopSourceURL();
+			List<String> targetURLs = webInfo.getHadoopTargetURLs();
+
+			if (linkIndexMap.containsKey(sourceURL)) {
+				for (String targetURL : targetURLs) {
+					if (linkIndexMap.containsKey(targetURL)) {
+						adjacency[linkIndexMap.get(sourceURL)][linkIndexMap.get(targetURL)] = 1;
+					}
+				}
+			}
+		}
+
+		return adjacency;
+	}
+
+	/**
+	 * Gets the page rank map
+	 * 
+	 * @param linkIndexMap The link index map
+	 * @param adjacency    The adjacency matrix
+	 * @return The page rank map
+	 */
+	private HashMap<String, Double> getPageRankMap(HashMap<String, Integer> linkIndexMap, int[][] adjacency) {
+
+		int totalLinks = linkIndexMap.size();
+		double[] pageRanks = new double[totalLinks];
+		for (int i = 0; i < totalLinks; i++) {
+			pageRanks[i] = 1.0d;
+		}
+
+		// Calculating the page ranks
+		for (int i = 0; i < totalLinks; i++) {
+			int totalOutgoings = 0;
+			for (int j = 0; j < totalLinks; j++) {
+				if (adjacency[i][j] == 1) {
+					totalOutgoings++;
+				}
+			}
+
+			if (totalOutgoings > 0) {
+				for (int j = 0; j < totalLinks; j++) {
+					if (adjacency[i][j] == 1) {
+						pageRanks[j] += pageRanks[i] / totalOutgoings;
+					}
+				}
+			}
+		}
+
+		// Preparing the map
+		HashMap<String, Double> pageRankMap = new HashMap<>();
+		for (Entry<String, Integer> entry : linkIndexMap.entrySet()) {
+			pageRankMap.put(entry.getKey(), pageRanks[entry.getValue()]);
+		}
+
+		return pageRankMap;
+	}
+
+	/**
+	 * Gets the page rank map
+	 * 
+	 * @param webInfoList The web info list
+	 * @return The pagerank map
+	 */
+	private HashMap<String, Double> getPageRankMap(List<WebInfo> webInfoList) {
+
+		HashMap<String, Integer> linkIndexMap = this.getLinkIndexMap(webInfoList);
+		int[][] adjacency = this.prepareAdjacencyMatrix(webInfoList, linkIndexMap);
+
+		// Removing the loops
+		adjacency = this.removeLoops(adjacency);
+		return this.getPageRankMap(linkIndexMap, adjacency);
+	}
+
+	/**
+	 * Removes the loops
+	 * 
+	 * @param adjacencyMatrix The adjacency matrix
+	 */
+	private int[][] removeLoops(int[][] adjacencyMatrix) {
+
+		LoopDetector loopDetector = new LoopDetector();
+		List<Connection> loops = loopDetector.getLoops(adjacencyMatrix);
+		for (Connection connection : loops) {
+			adjacencyMatrix[connection.getSourceLinkIndex()][connection.getTargetLinkIndex()] = 0;
+		}
+
+		return adjacencyMatrix;
+	}
+
+	/**
+	 * Reduces the web info list
+	 */
+	@Override
+	public void reduce(String key, Iterator<WebInfo> values, OutputCollector<String, Double> output, Reporter reporter)
+			throws IOException {
+
+		List<WebInfo> webInfoList = new ArrayList<>();
+		while (values.hasNext()) {
+			webInfoList.add(values.next());
+		}
+
+		HashMap<String, Double> pagerankMap = this.getPageRankMap(webInfoList);
+		for (Entry<String, Double> entry : pagerankMap.entrySet()) {
+			output.collect(entry.getKey(), entry.getValue());
+		}
+	}
+}
